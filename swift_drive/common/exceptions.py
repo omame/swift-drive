7085451380b4739f2b7df711704df954fed3c384
Marcelo Martins <marcelo.martins@rackspace.com>
2013-04-12 12:55:37 -0500

    adding initial exceptions module

---
+++ swift_drive/common/exceptions.py
@@ -0,0 +1,24 @@
+""" See COPYING for license information """
+
+
+class Error(StandardError):
+    """
+    Base class for all errors and exceptions
+    """
+    pass
+
+
+class ResponseError(Error):
+    """
+    A General response error
+    """
+    def __init__(self, status, reason):
+        self.status = status
+        self.reason = reason
+        Error.__init__(self)
+
+    def __str__(self):
+        return '%d - %s' % (self.status, self.reason)
+
+    def __repr__(self):
+        return '%d - %s' % (self.status, self.reason)
